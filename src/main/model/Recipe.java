39bd88e8f64259ddeca28d6668bfd0a1feb073e6
mishc <mishra.chirag2879@gmail.com>
2023-10-09 17:37:02 -0700

    added getters

---
+++ src/main/model/Recipe.java
@@ -0,0 +1,40 @@
+package model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Recipe {
+    private List<String> ingredients;
+    private String cuisine;
+    private String name;
+
+    public Recipe(String name, String cuisine) {
+        this.name = name;
+        this.cuisine = cuisine;
+        ingredients = new ArrayList<>();
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getCuisine() {
+        return cuisine;
+    }
+
+    public List<String> getIngredients() {
+        return ingredients;
+    }
+
+    public void addIngredient(String ingredient) {
+        if (!ingredients.contains(ingredient)) {
+            ingredients.add(ingredient);
+        }
+    }
+
+    public void removeIngredient(String ingredient) {
+        if (ingredients.contains(ingredient)) {
+            ingredients.remove(ingredient);
+        }
+    }
+}
---
+++ src/main/model/RecipeBook.java
@@ -0,0 +1,45 @@
+package model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class RecipeBook {
+
+    private String userName;
+    private List<Recipe> recipes;
+
+    public RecipeBook(String name) {
+        this.userName = name;
+        recipes = new ArrayList<>();
+    }
+
+    public String getName() {
+        return userName;
+    }
+
+    public List<String> getRecipes() {
+        List<String> recipesName = new ArrayList<>();
+        for (Recipe i : recipes) {
+            recipesName.add(i.getName());
+        }
+        return recipesName;
+    }
+
+    public void addRecipe(Recipe r) {
+        if (!recipes.contains(r)) {
+            recipes.add(r);
+        }
+    }
+
+    public void removeRecipe(Recipe r) {
+        if (recipes.contains(r)) {
+            recipes.remove(r);
+        }
+    }
+
+
+
+
+
+
+}
