611465a1989c2764725ca9cbbfaf1d7fb1c93997
mishc <mishra.chirag2879@gmail.com>
2023-10-25 12:42:25 -0700

    Completed Phase 2. v2

--- src/test/persistence/JsonWriterTest.java
+++ src/test/persistence/JsonWriterTest.java
@@ -6,6 +6,7 @@ import persistence.JsonReader;
 import persistence.JsonWriter;
 
 import java.io.IOException;
+import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -48,9 +49,19 @@ public class JsonWriterTest extends JsonTest {
             RecipeBook wr = new RecipeBook();
             Recipe r1 = new Recipe("abc", "def");
             Recipe r2 = new Recipe("def", "xyz");
+            r1.addIngredient("water");
+            r2.addIngredient("pasta");
+            r1.addCookingInstruction("boil water", 1);
+            r2.addCookingInstruction("add pasta", 2);
             wr.addRecipe(r1);
             wr.addRecipe(r2);
             ArrayList<Recipe> recipes = new ArrayList<>();
+            ArrayList<CookingInstructions> instructions = new ArrayList<>();
+            ArrayList<String> ingredients = new ArrayList<>();
+            ingredients.add("water");
+            ingredients.add("pasta");
+            instructions.add(new CookingInstructions("boil water", 1));
+            instructions.add(new CookingInstructions("add pasta", 2));
             recipes.add(new Recipe("abc", "def"));
             recipes.add(new Recipe("def", "xyz"));
             JsonWriter writer = new JsonWriter("./data/testWriterGeneral.json");
@@ -61,6 +72,10 @@ public class JsonWriterTest extends JsonTest {
             JsonReader reader = new JsonReader("./data/testWriterGeneral.json");
             wr = reader.read();
             List<Recipe> recipes1 = wr.getRecipes();
+            assertEquals("boil water", recipes1.get(0).getCookingInstructions().get(0).getInstruction());
+            assertEquals("add pasta", recipes1.get(1).getCookingInstructions().get(0).getInstruction());
+            assertEquals("water", recipes1.get(0).getIngredients().get(0));
+            assertEquals("pasta", recipes1.get(1).getIngredients().get(0));
             assertEquals(2, recipes1.size());
             checkRecipe("abc", recipes1.get(0), "def");
         } catch (IOException e) {
